a3929da35518b542853b9eb62468ce1496f2782f
Pankaj Bishnoi <152254295+Pankajbishnoi2@users.noreply.github.com>
2025-04-23 21:05:58 +0530

    Delete phpwebsite directory

--- phpwebsite/navbar2.php
+++
@@ -1,7 +0,0 @@
-<!-- navbar.php -->
-<nav style="background-color: #0056b3; padding: 10px; border-radius:12px;">
-    <a href="index.html" style="color: white; text-decoration: none; font-weight: bold; margin-right: 20px;">Home</a>
-   
-            
-       
-</nav>
